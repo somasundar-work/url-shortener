8b6a740555ff03aee471ef83f0de2feadbb3b944
Somasundar <somasundar.work@outlook.com>
2025-03-08 18:47:36 +0530

    Angular Setup with material css and tailwind css

---
+++ frontend/src/app/layout/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'url-shortener-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
